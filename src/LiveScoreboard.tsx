85d341430e2e1ce191ce9b00aaa0830d917fa704
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-02-22 16:58:22 +0100

    implemented finish game functionality

--- src/LiveScoreboard.tsx
+++ src/LiveScoreboard.tsx
@@ -30,6 +30,11 @@ const LiveScoreboard = () => {
     refreshGames();
   };
 
+  const finishGame = (gameId: string) => {
+    scoreboardManager.removeGame(gameId);
+    refreshGames();
+  };
+
   const refreshGames = () => {
     setGames(scoreboardManager.games);
   };
@@ -58,26 +63,33 @@ const LiveScoreboard = () => {
           Start Game
         </button>
       </div>
-      <div className="flex flex-col gap-2 my-2">
+      <div className="flex flex-col gap-2 my-2 ">
         {games.map((game) => (
           <div
             key={game.id}
-            className="flex flex-row gap-2 p-4 border border-gray-400 rounded-lg items-center hover:bg-gray-200 hover:border-gray-600"
+            className="flex flex-row gap-2 p-4 border border-gray-400 rounded-lg items-center justify-between hover:bg-gray-200 hover:border-gray-600"
           >
             <div>
               Home Team: {game.homeTeam.name} - {game.homeTeam.score}, Away
               Team: {game.awayTeam.name} - {game.awayTeam.score}
             </div>
-            <button
-              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white  bg-gray-400  border border-transparent rounded-md"
-              onClick={() => {
-                setSelectedGame(game);
-                setShowModal(true);
-              }}
-            >
-              Update game
-            </button>
-
+            <div className="mr-2 space-x-2">
+              <button
+                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white  bg-gray-400  border border-transparent rounded-md"
+                onClick={() => {
+                  setSelectedGame(game);
+                  setShowModal(true);
+                }}
+              >
+                Update game
+              </button>
+              <button
+                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white  bg-gray-400  border border-transparent rounded-md"
+                onClick={() => finishGame(game.id)}
+              >
+                Finish game
+              </button>
+            </div>
             {showModal &&
               selectedGame &&
               createPortal(
